6d50838691b78afa0ae39fc9981d49fe43ffb92c
Maiiialen <magdalena.salega@gmail.com>
2023-04-26 16:13:13 +0200

    refactor: move inicial functionality to components

---
+++ src/components/settings/Settings.tsx
@@ -0,0 +1,21 @@
+import "./Settings.scss"
+
+function Settings() {
+  return (
+    <div className="settings">
+      <div>settingsImage</div>
+      1+0 Bullet
+      2+1 Bullet
+      3+0 Blitz
+      3+2 Blitz
+      5+0 Blitz
+      5+3 Blitz
+      10+0 Rapid
+      10+5 Rapid
+      15+10 Rapid
+      30+0 Classical
+      30+20 Classical
+    </div>
+  )
+}
+export default Settings
