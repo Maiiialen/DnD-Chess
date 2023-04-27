3e66eaa773ee667fb3a847361dc4e39d19b1bed1
Maiiialen <magdalena.salega@gmail.com>
2023-04-27 17:15:23 +0200

    feat: declare starting pions positions

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -5,14 +5,13 @@ import useBoardStore from "../../store/Store"
 import Cell from "../cell/Cell"
 import "./Board.scss"
 
-function movePiece(name: string, row: number, column: number){
-    console.log("heree!", row, column);
-    return null
+function movePiece(name: string, row: number, column: number) {
+  console.log("heree!", row, column);
+  return null
 }
 
 function Board() {
-    const pieces = useBoardStore((state) => state.pieces)
-  const [boardState, setBoardState] = useState(["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR", "bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP", "wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"])
+  const pieces = useBoardStore((state) => state.pieces)
   return (
     <div className="board">
       <DndProvider backend={TouchBackend}>
@@ -20,10 +19,10 @@ function Board() {
           Array.from(Array(8)).map((_, column) => {
             {
               const newPiece = pieces.find((piece, idx) => piece.row === row && piece.column === column)
-              if(newPiece !== undefined) {
-                return <Cell key={"cell" + row*column} piece={newPiece} />
+              if (newPiece !== undefined) {
+                return <Cell key={"cell" + row * 8 + column} piece={newPiece} />
               } else {
-                return <Cell key={"cell" + row*column} piece={{name:"", row:row, column:column, type:undefined}} />
+                return <Cell key={"cell" + row * 8 + column} piece={{ name: "", row: row, column: column, type: undefined }} />
               }
             }
           })
--- src/store/Store.ts
+++ src/store/Store.ts
@@ -7,10 +7,196 @@ const useBoardStore = create<BoardStore>()(
     pieces: [
       {
         row: 0,
+        column: 0,
+        name: "bR",
+        type: "ROOK"
+      },
+      {
+        row: 0,
+        column: 1,
+        name: "bN",
+        type: "KNIGHT"
+      },
+      {
+        row: 0,
         column: 2,
         name: "bB",
-        type: "bishop"
-      }
+        type: "BISHOP"
+      },
+      {
+        row: 0,
+        column: 3,
+        name: "bQ",
+        type: "QUEEN"
+      },
+      {
+        row: 0,
+        column: 4,
+        name: "bK",
+        type: "KING"
+      },
+      {
+        row: 0,
+        column: 5,
+        name: "bB",
+        type: "BISHOP"
+      },
+      {
+        row: 0,
+        column: 6,
+        name: "bN",
+        type: "KNIGHT"
+      },
+      {
+        row: 0,
+        column: 7,
+        name: "bR",
+        type: "ROOK"
+      },
+      {
+        row: 1,
+        column: 0,
+        name: "bP",
+        type: "PAWN"
+      },
+      {
+        row: 1,
+        column: 1,
+        name: "bP",
+        type: "PAWN"
+      },
+      {
+        row: 1,
+        column: 2,
+        name: "bP",
+        type: "PAWN"
+      },
+      {
+        row: 1,
+        column: 3,
+        name: "bP",
+        type: "PAWN"
+      },
+      {
+        row: 1,
+        column: 4,
+        name: "bP",
+        type: "PAWN"
+      },
+      {
+        row: 1,
+        column: 5,
+        name: "bP",
+        type: "PAWN"
+      },
+      {
+        row: 1,
+        column: 6,
+        name: "bP",
+        type: "PAWN"
+      },
+      {
+        row: 1,
+        column: 7,
+        name: "bP",
+        type: "PAWN"
+      },
+      {
+        row: 6,
+        column: 0,
+        name: "wP",
+        type: "PAWN"
+      },
+      {
+        row: 6,
+        column: 1,
+        name: "wP",
+        type: "PAWN"
+      },
+      {
+        row: 6,
+        column: 2,
+        name: "wP",
+        type: "PAWN"
+      },
+      {
+        row: 6,
+        column: 3,
+        name: "wP",
+        type: "PAWN"
+      },
+      {
+        row: 6,
+        column: 4,
+        name: "wP",
+        type: "PAWN"
+      },
+      {
+        row: 6,
+        column: 5,
+        name: "wP",
+        type: "PAWN"
+      },
+      {
+        row: 6,
+        column: 6,
+        name: "wP",
+        type: "PAWN"
+      },
+      {
+        row: 6,
+        column: 7,
+        name: "wP",
+        type: "PAWN"
+      },
+      {
+        row: 7,
+        column: 0,
+        name: "wR",
+        type: "ROOK"
+      },
+      {
+        row: 7,
+        column: 1,
+        name: "wN",
+        type: "KNIGHT"
+      },
+      {
+        row: 7,
+        column: 2,
+        name: "wB",
+        type: "BISHOP"
+      },
+      {
+        row: 7,
+        column: 3,
+        name: "wQ",
+        type: "QUEEN"
+      },
+      {
+        row: 7,
+        column: 4,
+        name: "wK",
+        type: "KING"
+      },
+      {
+        row: 7,
+        column: 5,
+        name: "wB",
+        type: "BISHOP"
+      },
+      {
+        row: 7,
+        column: 6,
+        name: "wN",
+        type: "KNIGHT"
+      },
+      {
+        row: 7,
+        column: 7,
+        name: "wR",
+        type: "ROOK"
+      },
     ],
     movePiece: () => null,
     beatPiece: () => null,
