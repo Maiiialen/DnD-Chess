8ede382fcf90b9fa751f8b3c903b6170967e0a78
Maiiialen <magdalena.salega@gmail.com>
2023-05-02 16:32:31 +0200

    fix: fix images

--- src/store/interface.ts
+++ src/store/interface.ts
@@ -3,18 +3,7 @@ import { PieceType } from "../components/properties/interfaces"
 export interface BoardStore {
   pieces: PieceType[],
   images: {
-    bB: string,
-    bK: string,
-    bN: string,
-    bP: string,
-    bQ: string,
-    bR: string,
-    wB: string,
-    wK: string,
-    wN: string,
-    wP: string,
-    wQ: string,
-    wR: string,
+    [key: string]:string,
   }
   movePiece: (piece: PieceType, row: number, column: number) => void,
   beatPiece: () => null,
